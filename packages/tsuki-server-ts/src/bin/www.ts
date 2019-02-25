bb9ee86b8bc98573c4765e48f534290ebc1c53e7
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-02-25 01:14:36 -0300

    fix: Cleaned up Entry point

--- packages/tsuki-server-ts/src/bin/www.ts
+++ packages/tsuki-server-ts/src/bin/www.ts
@@ -1,15 +1,7 @@
-#!/usr/bin/env node
-
-/**
- * Node Module dependencies.
- */
-import debug from 'debug';
-import http from 'http';
-
 /**
  * Local Module dependencies.
  */
-import TsukiServer from '../app';
+import { TsukiServer } from '../app';
 import { APP_PORT } from '../config';
 
 const main = async (): Promise<void> => {
@@ -17,19 +9,12 @@ const main = async (): Promise<void> => {
    * Get port from environment and store in Express.
    */
   const port = normalizePort(APP_PORT);
-  TsukiServer.set('port', port);
+  const app = new TsukiServer(port);
 
   /**
-   * Create HTTP server.
+   * Listen on http.
    */
-  const httpServer = await http.createServer(TsukiServer);
-
-  /**
-   * Listen on provided port, on all network interfaces.
-   */
-  await httpServer.listen(port, () => console.log(`Server started @ http://localhost:${port}`));
-  httpServer.on('error', onError);
-  httpServer.on('listening', onListening);
+  await app.listen();
 
   /**
    * Normalize a port into a number, string, or false.
@@ -44,39 +29,6 @@ const main = async (): Promise<void> => {
     } else {
       return false;
     }
-  }
-
-  /**
-   * Event listener for HTTP server "error" event.
-   */
-
-  function onError(error: NodeJS.ErrnoException): void {
-    if (error.syscall !== 'listen') {
-      throw error;
-    }
-    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
-    switch (error.code) {
-      case 'EACCES':
-        console.error(`${bind} requires elevated privileges`);
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(`${bind} is already in use`);
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
-  }
-
-  /**
-   * Event listener for HTTP server "listening" event.
-   */
-
-  function onListening(): void {
-    const addr = httpServer.address();
-    const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
-    debug(`Listening on ${bind}`);
   }
 };
 
