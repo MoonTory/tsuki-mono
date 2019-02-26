3cb1812c6f570256f0174f237db0c9b39e3d971b
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-02-25 22:53:34 -0300

    fix: Fixed Mongo Connection Function

--- packages/tsuki-server-ts/src/infra/db/mongo/connectMongo/index.ts
+++ packages/tsuki-server-ts/src/infra/db/mongo/connectMongo/index.ts
@@ -1,14 +1,11 @@
 import mongoose from 'mongoose';
 
-// DB Config
-import { MONGO_URI } from '../../../../config';
-
-export default async (): Promise<void> => {
+export const connectMongo = async (CONNECTION_URI: string): Promise<void> => {
   try {
     // Fix Depreication Warning
     mongoose.set('useFindAndModify', false);
     // Connect to MongoDB
-    await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+    await mongoose.connect(CONNECTION_URI, { useNewUrlParser: true });
     console.log('MongoDB Connected...');
   } catch (exception) {
     // Log Exception
