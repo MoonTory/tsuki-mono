00847b9823b63b80e2879abc4ea99b34c5bd1afa
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-02-27 21:47:24 -0300

    fix: Fixed importAllControllers function name
    - Fixed the function names, and also added an extra function named impor
    tController, this function allows the user to import a specific controll
    er

--- packages/tsuki-server-ts/src/interfaces/http/helpers/importControllers/index.ts
+++ packages/tsuki-server-ts/src/interfaces/http/helpers/importControllers/index.ts
@@ -1,14 +1,19 @@
 import path from 'path';
-import { IController } from '../../../../typings/http/controller';
 
-export default async function createControllerRoutes(modules: string[]): Promise<IController[]> {
+import { IController } from '../../../../typings/http/controller';
+import { parseDirFolderNames } from '../parseDirFolderNames';
+
+export async function importAllControllers(): Promise<IController[]> {
   try {
+    const modules: string[] = await parseDirFolderNames('src/interfaces/http/routers/');
+
     const controllers: IController[] = [];
 
-    modules.forEach(controllerUri => {
-      let controllerPath = path.resolve('src/interfaces/http/controllers/', controllerUri);
-      let controller: IController = require(controllerPath);
-
+    modules.forEach(controllerURI => {
+      let dirPath: string = 'src/interfaces/http/routers/' + controllerURI;
+      let controllerPath: string = path.resolve(dirPath);
+      let Controller = require(controllerPath);
+      let controller = new Controller.default(controllerURI);
       controllers.push(controller);
     });
 
@@ -17,3 +22,18 @@ export default async function createControllerRoutes(modules: string[]): Promise
     throw error;
   }
 }
+
+export const importController = (controllerUri: string) => {
+  try {
+    let controllerPath = path.resolve('src/interfaces/http/controllers/', controllerUri);
+    let cntrl = require(controllerPath);
+
+    console.log('cntrl', cntrl);
+
+    const controller = new cntrl.default(controllerUri);
+
+    return controller;
+  } catch (error) {
+    throw error;
+  }
+};
