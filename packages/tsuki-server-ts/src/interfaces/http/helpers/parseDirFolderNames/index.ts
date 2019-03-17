9aa0b3772f15e530352057966036b47b1b9e6fa6
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-03-17 01:56:31 -0300

    documentation: Added docs for parseDirFolderNames function

--- packages/tsuki-server-ts/src/interfaces/http/helpers/parseDirFolderNames/index.ts
+++ packages/tsuki-server-ts/src/interfaces/http/helpers/parseDirFolderNames/index.ts
@@ -1,9 +1,11 @@
 import { readdirSync, statSync } from 'fs';
 import { join } from 'path';
 
-// Helper function to get directory names from a specified path
-// @params -> pathName: string
-// Provide a pathName in string format to the directory in which you want to parse the folders of.
-
+/**
+ * Helper function to get directory names from a specified path, provide a pathName in string format relative to the
+ * working directory, to the directory in which you want to parse the folders of.
+ * @param pathName: string
+ * @returns string[]
+ */
 export const parseDirFolderNames = (pathName: string): string[] =>
   readdirSync(pathName).filter((dirName: string) => statSync(join(pathName, dirName)).isDirectory());
